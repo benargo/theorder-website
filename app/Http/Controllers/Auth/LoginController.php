8d6d0277fccd4eff98c3bbe83b07ad0ce03d8287
Ben Argo <ben@benargo.com>
2018-03-03 13:36:51 +0000

    Add Battle.net authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,30 +2,34 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use SocialiteProviders\Manager\Config;
+use Laravel\Socialite\Facades\Socialite;
 
+/**
+ * Login Controller
+ *
+ * This controller handles authenticating users for the application and
+ * redirecting them to your home screen. The controller uses a trait
+ * to conveniently provide its functionality to your applications.
+ */
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+
+    /**
+     * Contains the socialite driver instance.
+     *
+     * @var Illuminate\Support\Manager
+     */
+    protected $driver;
 
     /**
      * Create a new controller instance.
@@ -34,6 +38,57 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        // Configure the Socialite driver to use the EU region.
+        $client_id = config('services.battlenet.client_id');
+        $client_secret = config('services.battlenet.client_secret');
+        $redirect = config('services.battlenet.redirect');
+        $additional_config = ['region' => 'eu'];
+        $config = new Config($client_id, $client_secret, $redirect, $additional_config);
+        $this->driver = Socialite::driver('battlenet')->setConfig($config);
+    }
+
+    /**
+     * Redirect the user to the Battle.net authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return $this->driver
+            ->scopes(['wow.profile'])
+            ->redirect();
+    }
+
+    /**
+     * Obtain the user information from Battle.net.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        // Fetch details about the user from Battle.net...
+        $response = $this->driver->user();
+
+        // Check the database for a user model, or create one if one doesn't
+        // exist...
+        $user = User::firstOrCreate(
+            ['id' => $response->id],
+            [
+                'battletag' => $response->nickname,
+                'access_token' => $response->token,
+            ]
+        );
+
+        // Login and "remember" the given user...
+        Auth::login($user, true);
+
+        // If there is no character selected, redirect them to the character
+        // select page...
+        // if (is_null($user->main_character)) {
+        //     return redirect()->route('character-select');
+        // }
+
+        // Otherwise, return them to the home page...
+        return redirect($this->redirectTo);
     }
 }
