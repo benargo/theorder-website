0e0f9d2d6be1a305851ef2dbdf76b5e02011870c
Ben Argo <ben@benargo.com>
2018-09-07 15:57:20 +0100

    Change namespace of Warcraft classes.

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Battlenet\Regions;
+use App\Warcraft\Regions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
--- app/Warcraft/Regions.php
+++ app/Warcraft/Regions.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Battlenet;
+namespace App\Warcraft;
 
 trait Regions
 {
