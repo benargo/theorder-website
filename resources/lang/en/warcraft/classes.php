18928f33829d1855ee7ceb5fdfbd36ad2dfbea92
Ben Argo <ben@benargo.com>
2018-12-02 07:56:21 +0000

    Add WoW class language lines and icons

---
+++ resources/lang/en/warcraft/classes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Warcraft Classes Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by World of Warcraft classes.
+    |
+    */
+
+   'druid' => 'druid|druids',
+   'hunter' => 'hunter|hunters',
+   'mage' => 'mage|magi',
+   'paladin' => 'paladin|paladins',
+   'priest' => 'priest|priests',
+   'rogue' => 'rogue|rogues',
+   'warlock' => 'warlock|warlocks',
+   'warrior' => 'warrior|warriors',
+
+];
