b71bc8d86e5ed396278e1b43e679368bc3d1916d
Ben Argo <ben@benargo.com>
2019-07-02 21:37:29 +0200

    Disable notifications link for the timebeing.

--- resources/assets/js/components/NotificationsMenuItem.vue
+++ resources/assets/js/components/NotificationsMenuItem.vue
@@ -8,7 +8,7 @@
     <div class="dropdown-menu dropdown-menu-right bg-brown" aria-labelledby="notificationsDropdown">
         <a v-for="notification in notifications" class="dropdown-item nav-link" :href="getNotificationUrl(notification)">{{ notification.data.title }}</a>
         <div v-show="notifications.length >= 1" class="dropdown-divider"></div>
-        <a class="dropdown-item nav-link" href="/notifications">
+        <a class="dropdown-item nav-link disabled" href="/notifications" tabindex="-1" aria-disabled="true">
             <font-awesome-icon :icon="['far', 'history']"></font-awesome-icon>
             {{ lang.notifications_history }}
         </a>
