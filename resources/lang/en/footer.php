7f24dbe056659f53fc0f24bfc5fba2d5fb2b575b
Ben Argo <ben@benargo.com>
2019-07-02 15:15:00 +0200

    Modify footer disclaimer

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -12,10 +12,10 @@ return [
     */
 
     'creator'    => 'Website by Ben Argo',
-    'disclaimer' => '<strong>Disclaimer:</strong> Classic is a trademark, and World of Warcraft and Warcraft are trademarks or registered trademarks of Blizzard Entertainment, Inc., in the U.S. and/or other countries. All related materials, logos, and images are copyright &copy; Blizzard Entertainment, Inc.. The Order is in no way associated with or endorsed by Blizzard Entertainment.',
-    'privacy'   => 'Your Data',
-    'battlenet' => 'Battle.net Usage Information',
-    'arr'       => 'All Rights Reserved.',
-    'promotion' => 'Website hosted by <strong>CMT Cloud<span class="cmtcloud-period">.</span></strong>',
+    'disclaimer' => 'Disclaimer: Classic is a trademark, and World of Warcraft and Warcraft are trademarks or registered trademarks of Blizzard Entertainment, Inc., in the U.S. and/or other countries. All related materials, logos, and images are copyright &copy; Blizzard Entertainment, Inc.. The Order is in no way associated with or endorsed by Blizzard Entertainment.',
+    'privacy'    => 'Your Data',
+    'battlenet'  => 'Battle.net Usage Information',
+    'arr'        => 'All Rights Reserved.',
+    'promotion'  => 'Website hosted by <strong>CMT Cloud<span class="cmtcloud-period">.</span></strong>',
 
 ];
