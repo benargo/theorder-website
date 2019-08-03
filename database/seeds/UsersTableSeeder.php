4fe5cc9cb231ed1684d234e1f0b956fd864d109d
Ben Argo <ben@benargo.com>
2019-08-03 22:16:33 +0100

    Add Tommy to list of default administrators

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,18 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'id' => 1922840,
-            'nickname' => 'Saromius',
-            'battletag' => encrypt('Animorphus#2491'),
-            'rank_id' => 1,
+            [
+                'id' => 1922840,
+                'nickname' => 'Saromius',
+                'battletag' => encrypt('Animorphus#2491'),
+                'rank_id' => 1,
+            ],
+            [
+                'id' => 51425995,
+                'nickname' => 'Ninjaa',
+                'battletag' => encrypt('Ninjaa1337#2904'),
+                'rank_id' => 1,
+            ],
         ]);
     }
 }
