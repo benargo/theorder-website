f9ca6474d204a3b72e21da3f708bf2eac7542424
Ben Argo <ben@benargo.com>
2019-08-11 03:36:11 +0100

    Add Stock migration

---
+++ database/migrations/2019_08_11_033500_create_guild_bank_stock_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuildBankStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guild_bank_stock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('banker_name', 12);
+            $table->boolean('is_in_bags');
+            $table->integer('bag_number')->nullable();
+            $table->integer('slot_number');
+            $table->integer('item_id');
+            $table->integer('count');
+            $table->timestamps();
+            $table->unsignedInteger('updated_by_user_id');
+            $table->dateTime('withdrawn_at')->nullable();
+
+            // Set up the foreign key to the users table...
+            $table->foreign('updated_by_user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guild_bank_stock');
+    }
+}
