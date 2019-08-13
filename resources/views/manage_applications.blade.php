3bfbdb7acb279f71ea1fb543c93e0cca6152a6c8
Ben Argo <ben@benargo.com>
2019-08-14 00:05:34 +0100

    Increase header size

--- resources/views/manage_applications.blade.php
+++ resources/views/manage_applications.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div id="app">
-        <header class="container-fluid bg-engineering py-6 text-light">
+        <header class="container-fluid bg-engineering py-7 text-light">
             <div class="content">
                 <h1 class="text-center">{{ __('controlpanel.applications') }}</h1>
             </div>
