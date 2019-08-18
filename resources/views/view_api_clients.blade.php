a723f9307f35a7cabd9073b5ae9215f9aea375a4
Ben Argo <ben@benargo.com>
2019-08-18 14:30:48 +0100

    Update language
    We’re slowly moving away from using the translation feature.

--- resources/views/view_api_clients.blade.php
+++ resources/views/view_api_clients.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
     <div id="app" class="page-control-panel-index">
-        <div class="container-fluid bg-engineering py-7">
+        <header class="container-fluid bg-engineering py-7">
             <div class="content">
                 <h1 class="text-center">
-                    {{ __('controlpanel.stock_addon_settings') }}
+                    Stock Addon Settings
                 </h1>
             </div>
-        </div>
+        </header>
         <div class="container py-7">
             <p class="lead mb-5">In order to use the Stock addon, you first need to create an OAuth Client. This is required in order to authenticate with the API.</p>
             <passport-clients></passport-clients>
