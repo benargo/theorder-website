ae12440d4d00dbe160f27edfd1989c7b3f03f035
Ben Argo <ben@benargo.com>
2020-01-19 16:49:16 +0000

    Add job for closing raid signups
    For now this is empty, but it’s coming. I promise…

---
+++ app/Jobs/CloseRaidSignups.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Raiding\Raid;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class CloseRaidSignups implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The raid which we're closing signups for.
+     *
+     * @var App\Raiding\Raid
+     */
+    protected $raid;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Raid $raid)
+    {
+        $this->raid = $raid;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
