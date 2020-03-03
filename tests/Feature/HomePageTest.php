e63ccb2d751a1fb6b43108124e48e52503ed8116
Ben Argo <ben@benargo.com>
2020-03-03 13:44:20 +0000

    Add homepage test

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HomePageTest extends TestCase
+{
+    /**
+     * Test the page response.
+     *
+     * @return void
+     */
+    public function testResponse()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
