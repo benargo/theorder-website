59fe5d94c426f4a945574a0ef055bf7d1c918ce3
Ben Argo <ben@benargo.com>
2020-04-09 11:24:23 +0100

    Add first test case for Dusk

---
+++ tests/Browser/SeeBankersTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class SeeBankersTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testSeeBankers()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/bank')
+                    ->assertSee('I want to donate items...')
+                    ->press('I want to donate items...')
+                    ->assertSee('Below you will find the list of bank characters, so you know where to send each item.');
+        });
+    }
+}
